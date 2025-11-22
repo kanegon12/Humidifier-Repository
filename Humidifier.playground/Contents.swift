89639f6c28d7a73258610c925aa956201436c918
kanegon12 <swift.newbie01@gmail.com>
2025-11-23 01:15:53 +0900

    Humidifier.playgroundコード再修正

--- Humidifier.playground/Contents.swift
+++ Humidifier.playground/Contents.swift
@@ -17,48 +17,66 @@ class Humidifier {
             repeats: true // 繰り返し処理を実行したい場合はtrue
         )
     }
+    
+    var waterRemaining: Int = 5 // 現在の水量 mL
+    var waterTankRemaining: Int {
+        humidifier.waterTank - waterRemaining
+    }// タンクの空き容量 mL
+    
     // Timerクラスに設定するメソッドは「＠objc」をつける
     @objc func countUp() {
         // Countで増減する値の設定
         count += 1
         print("加湿器が起動してから\(count)秒経過しました")
         // 1秒ごとに水が減る
-        if humidifier.waterRemaining > 0 {
-            humidifier.waterRemaining = humidifier.waterRemaining - quantityDecreasePer1Seconds
-            if humidifier.waterRemaining == 0 {
+        if self.waterRemaining > 0 {
+            self.waterRemaining = self.waterRemaining - quantityDecreasePer1Seconds
+            if self.waterRemaining == 0 {
                 print ("タンクが空になりました、加湿器を停止します")
                 timer?.invalidate()
+     
             }
         }
     }
     
-    // 加湿器
-    var humidifier = HumidifierModel(
-        humidity: 34,
-        waterTank: 300,
-        waterRemaining: 5,
-    )
-    // 加湿器情報
+//    // 加湿器
+//    var humidifier = HumidifierModel(
+//        humidity: 34,
+//        waterTank: 300,
+//       waterRemaining: 5,
+//    )
+//    // 加湿器情報
+//    struct HumidifierModel {
+//        let humidity: Int // 湿度 %
+//        let waterTank: Int // タンク総量 mL
+//        var waterRemaining: Int // 現在の水量 mL
+//        var waterTankRemaining: Int {
+//            waterTank - waterRemaining
+//        } // タンクの空き容量 mL
+//    }
+    
+    let humidifier = HumidifierModel(
+         humidity: 34,
+         waterTank: 300
+     )
+
     struct HumidifierModel {
-        let humidity: Int // 湿度 %
-        let waterTank: Int // タンク総量 mL
-        var waterRemaining: Int // 現在の水量 mL
-        var waterTankRemaining: Int {
-            waterTank - waterRemaining
-        } // タンクの空き容量 mL
-    }
+         let humidity: Int // 湿度 %
+         let waterTank: Int // タンク総量 mL
+     }
     // 給水量
     enum WaterSupply {
         case full
         case specifiedQuantity(Int)
         case none
         // 実際の給水量
-        func waterQuantity(water: HumidifierModel) -> Int {
+        func waterQuantity(waterTank: Int, waterRemaining: Int) -> Int {
+            let waterTankRemaining = waterTank - waterRemaining
             switch self {
             case .full:
-                return water.waterTankRemaining
+                return waterTankRemaining
             case .specifiedQuantity(let putQuantity):
-                let freeSpace = water.waterTankRemaining
+                let freeSpace = waterTankRemaining
                 if putQuantity > freeSpace {
                     return freeSpace
                 }
@@ -96,10 +114,16 @@ class Humidifier {
             return false
         }
         // 給水処理
-        let waterQuantity = supply.waterQuantity(water: humidifier)
-        humidifier.waterRemaining += waterQuantity
+//        let waterQuantity = supply.waterQuantity(tank: humidifier.waterTank, waterRemaining: self.waterRemaining)
         
-        guard humidifier.waterRemaining > 0 else {
+        let waterQuantity = supply.waterQuantity(
+            waterTank: humidifier.waterTank,
+                    waterRemaining: self.waterRemaining
+                )
+        
+        self.waterRemaining += waterQuantity
+        
+        guard self.waterRemaining > 0 else {
             print("タンクが空です、給水してください")
             return false
         }
